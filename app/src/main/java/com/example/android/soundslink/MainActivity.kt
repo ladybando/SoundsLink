994bbaf5f76b866b51c8981f2298aa9df45a5f72
Ayana Bando <ayanaflax@yahoo.com>
2021-10-05 09:48:59 -0400

    add songs info file

--- app/src/main/java/com/example/android/soundslink/MainActivity.kt
+++ app/src/main/java/com/example/android/soundslink/MainActivity.kt
@@ -4,16 +4,11 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.annotation.RequiresApi
-import com.example.android.soundslink.assets.FileHelper
-import java.io.File
-import java.nio.file.Paths
 
 class MainActivity : AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-       FileHelper().printText()
     }
 }
