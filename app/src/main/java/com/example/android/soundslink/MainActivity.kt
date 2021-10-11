5952872cffc69905a7926d46d2c74635460cb0c9
Ayana Bando <ayanaflax@yahoo.com>
2021-10-11 12:41:04 -0400

    update look of cards

--- app/src/main/java/com/example/android/soundslink/MainActivity.kt
+++ app/src/main/java/com/example/android/soundslink/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.android.soundslink.adapter.SongArtistListAdapter
 import com.example.android.soundslink.databinding.ActivityMainBinding
 import com.example.android.soundslink.model.SongsViewModel
 
@@ -15,21 +16,27 @@ class MainActivity : AppCompatActivity() {
     private lateinit var adapter: SongArtistListAdapter
     private val viewModel: SongsViewModel by viewModels()
 //creates window for ui
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        adapter = SongArtistListAdapter()
+        setUpRecyclerView()
         //todo figure out how to get viewModel list to display with picture and appropriate text
         //how can i associate singer with song?
+
+
+        //have view model functions that take an int and allow mainactivity to set resources per adapter item
+        // songItem = viewModel.getResourcefromResources(getResources().
+        //getIntArray(R.array.stamp_counter_array))
+
+    }
+
+    private fun setUpRecyclerView(){
         recyclerView = binding.recycleViewList
         adapter = SongArtistListAdapter()
-        recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
-
-    //have view model functions that take an int and allow mainactivity to set resources per adapter item
-    // songItem = viewModel.getResourcefromResources(getResources().
-    //getIntArray(R.array.stamp_counter_array))
-
-}
+        recyclerView.adapter = adapter
+    }
 }
--- app/src/main/java/com/example/android/soundslink/adapter/SongArtistListAdapter.kt
+++ app/src/main/java/com/example/android/soundslink/adapter/SongArtistListAdapter.kt
@@ -1,18 +1,14 @@
-package com.example.android.soundslink
+package com.example.android.soundslink.adapter
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.android.soundslink.R
 import com.example.android.soundslink.data.allArtistsList
 import com.example.android.soundslink.data.allSongsList
 import com.example.android.soundslink.databinding.SongItemLayoutBinding
 
-//should set how recyclerview items look
-//should functionality should be set to activity?
-//and data should be set to viewmodel?
 class SongArtistListAdapter : RecyclerView.Adapter<SongArtistListAdapter.SongViewHolder>() {
-    //as of right now only displaying the last song and artist in list
 
     private val song = allSongsList
     private val artist = allArtistsList
@@ -30,12 +26,9 @@ class SongArtistListAdapter : RecyclerView.Adapter<SongArtistListAdapter.SongVie
         return SongViewHolder(binding)
     }
 
-    //returning two songs now
     override fun onBindViewHolder(holder: SongViewHolder, position: Int) = with(holder) {
         artistTextView.text = song[position]
         songAlbumName.text = artist[position]
-        val song = song[position]
-        Log.d("Adapter song", song)
         albumArtImage.setImageResource(R.drawable.gta_vicecity_main)
     }
 
